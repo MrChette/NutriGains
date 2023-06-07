945f95cc1832fd9f3393d07fa26228cd8c8a5ea6
Chette <80092129+MrChette@users.noreply.github.com>
2023-06-07 21:58:38 +0200

    [UPDATE] login y register se mueven con el teclado

--- NutriGainsFront/nutrigains_front/lib/screens/login_screen.dart
+++ NutriGainsFront/nutrigains_front/lib/screens/login_screen.dart
@@ -1,22 +1,8 @@
-// ignore_for_file: use_build_context_synchronously
-
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_styled_toast/flutter_styled_toast.dart';
-import 'package:nutrigains_front/models/comment_model.dart';
-import 'package:nutrigains_front/models/food_model.dart';
-import 'package:nutrigains_front/services/food_service.dart';
-import 'package:nutrigains_front/services/recipeList_service.dart';
-import 'package:nutrigains_front/services/recipe_service.dart';
 import 'package:provider/provider.dart';
 
-import '../models/meal_model.dart';
-import '../models/recipe_model.dart';
 import '../providers/login_provider.dart';
 import '../services/auth_service.dart';
-import '../services/comment_service.dart';
-import '../services/meal_service.dart';
 import '../ui/input_decorations.dart';
 import '../widgets/CustomToast.dart';
 import '../widgets/auth_background.dart';
@@ -28,41 +14,50 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: AuthBackground(
-            child: SingleChildScrollView(
+      resizeToAvoidBottomInset: true,
+      body: AuthBackground(
+        child: SingleChildScrollView(
           child: Column(
             children: [
               const SizedBox(height: 250),
               CardContainer(
-                  child: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  const Text('Login',
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Login',
                       style: TextStyle(
                         fontSize: 30,
                         color: Colors.white,
-                      )),
-                  const SizedBox(height: 60),
-                  ChangeNotifierProvider(
-                      create: (_) => LoginFormProvider(), child: _LoginForm())
-                ],
-              )),
+                      ),
+                    ),
+                    const SizedBox(height: 60),
+                    ChangeNotifierProvider(
+                      create: (_) => LoginFormProvider(),
+                      child: _LoginForm(),
+                    ),
+                  ],
+                ),
+              ),
               const SizedBox(height: 50),
               TextButton(
-                  onPressed: () =>
-                      Navigator.pushReplacementNamed(context, 'register'),
-                  style: ButtonStyle(
-                      overlayColor: MaterialStateProperty.all(Colors.black),
-                      shape: MaterialStateProperty.all(const StadiumBorder())),
-                  child: const Text(
-                    'Crear una nueva cuenta',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  )),
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, 'register'),
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.black),
+                  shape: MaterialStateProperty.all(const StadiumBorder()),
+                ),
+                child: const Text(
+                  'Crear una nueva cuenta',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
               const SizedBox(height: 50),
             ],
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
 
@@ -76,183 +71,83 @@ class _LoginForm extends StatelessWidget {
       autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.amber,
-                width: 4,
-              ),
+          TextFormField(
+            autocorrect: false,
+            decoration: InputDecorations.authInputDecoration(
+              hintText: 'username',
+              labelText: 'Username',
+              prefixIcon: Icons.person,
             ),
-            child: TextFormField(
-              autocorrect: false,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: 'username',
-                  labelText: 'Username',
-                  prefixIcon: Icons.person),
-              onChanged: (value) => loginForm.username = value,
-            ),
+            onChanged: (value) => loginForm.username = value,
+            style: TextStyle(color: Colors.white, fontSize: 20),
           ),
-          const SizedBox(height: 60),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.amber,
-                width: 4,
-              ),
-            ),
-            child: Column(
-              children: [
-                TextFormField(
-                  autocorrect: false,
-                  obscureText: true,
-                  decoration: InputDecorations.authInputDecoration(
-                      hintText: '*****',
-                      labelText: 'Contraseña',
-                      prefixIcon: Icons.lock_outline),
-                  onChanged: (value) => loginForm.password = value,
-                  validator: (value) {
-                    return (value != null && value.length >= 5)
-                        ? null
-                        : 'La contraseña debe de ser mayor de 5 caracteres';
-                  },
+          const SizedBox(height: 40),
+          Column(
+            children: [
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '*****',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline,
                 ),
-                SizedBox(height: 10),
-              ],
+                onChanged: (value) => loginForm.password = value,
+                validator: (value) {
+                  return (value != null && value.length >= 5)
+                      ? null
+                      : 'La contraseña debe tener al menos 5 caracteres';
+                },
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+          const SizedBox(height: 30),
+          MaterialButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            disabledColor: Colors.black,
+            elevation: 1,
+            color: Colors.amber,
+            onPressed: loginForm.isLoading
+                ? null
+                : () async {
+                    FocusScope.of(context).unfocus();
+                    final authService =
+                        Provider.of<AuthService>(context, listen: false);
+
+                    if (!loginForm.isValidForm()) return;
+
+                    // validar si el login es correcto
+                    final String? data = await authService.login(
+                        loginForm.username, loginForm.password);
+                    final spliter = data?.split(',');
+                    loginForm.isLoading = true;
+                    if (spliter?[0] == '200') {
+                      if (spliter?[1] == 'ROLE_ADMIN') {
+                        Navigator.pushReplacementNamed(context, '');
+                      } else {
+                        Navigator.pushReplacementNamed(context, 'recipeScreen');
+                      }
+                    } else {
+                      CustomToast.customToast(
+                          'Email or password incorrect', context);
+                      Navigator.pushReplacementNamed(context, 'login');
+                    }
+                  },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+              child: Text(
+                loginForm.isLoading ? 'Espere' : 'Ingresar',
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black45,
+                ),
+              ),
             ),
           ),
-          const SizedBox(height: 70),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              disabledColor: Colors.black,
-              elevation: 1,
-              color: Colors.amber,
-              onPressed: loginForm.isLoading
-                  ? null
-                  : () async {
-                      FocusScope.of(context).unfocus();
-                      final authService =
-                          Provider.of<AuthService>(context, listen: false);
-
-                      if (!loginForm.isValidForm()) return;
-
-                      // validar si el login es correcto
-                      final String? data = await authService.login(
-                          loginForm.username, loginForm.password);
-                      final spliter = data?.split(',');
-                      loginForm.isLoading = true;
-                      if (spliter?[0] == '200') {
-                        if (spliter?[1] == 'ROLE_ADMIN') {
-                          Navigator.pushReplacementNamed(context, '');
-                        } else {
-                          Navigator.pushReplacementNamed(
-                              context, /*'commentScreen'*/ 'recipeScreen');
-                        }
-
-                        // * *  PRUEBA DE ENDPOINTS //
-
-                        //  NEWRECIPE //
-                        //await RecipeService().newRecipe("Prueba desde flutter 2");
-
-                        //  NEWFOOD //
-                        //final food = FoodModel(
-                        //    name: "Prueba desde flutter",
-                        //    carbohydrates: 100.0,
-                        //    fat: 100.0,
-                        //    kcal: 100.0,
-                        //    protein: 100.0,
-                        //    salt: 100.0,
-                        //    sugar: 100.0);
-                        //await FoodService().newFood(food);
-
-                        // NEWFOODBYBARCODE  //
-                        //FoodService().newFoodByApi(3168930009078);
-
-                        //  GETALLUSERFOOD //
-                        //List<FoodModel> foodList;
-                        //foodList = await FoodService().getAllUserFood();
-                        //foodList.forEach((food) {
-                        //  print(food.toString());
-                        //});
-
-                        //  NEWCOMMENT  //
-                        //await CommentService().newComment(1, "Probando un comentario desde flutter");
-
-                        // COMMENTBYIDRECIPE //
-                        //List<CommentModel> commentList;
-                        //commentList =
-                        //    await CommentService().commentByIdRecipe(1);
-                        //commentList.forEach((comment) {
-                        //  print(comment.toString());
-                        //});
-
-                        //  ADDFOODTOMEAL - LIST -  ADDRECIPETOMEAL //
-                        //if (mealResult != null) {
-                        //  await MealListService()
-                        //      .addFoodToMeal(mealResult.id, 105);
-                        //  await MealListService()
-                        //      .addRecipeToMeal(mealResult.id, 1);
-                        //} else {
-                        //  print('No se pudo crear la comida');
-                        //}
-
-                        // GETMEALISTBYIDMEAL //
-                        //List<MealListModel> mealList =
-                        //    await MealListService().getMealListByIdMeal(1);
-                        //mealList.forEach((meal) {
-                        //  print(meal.toString());
-                        //});
-
-                        //  NEWMEAL  //
-                        //MealModel? mealResult;
-                        //mealResult = await MealService().newMeal();
-
-                        // GETMEALBYID //
-                        //MealModel meal;
-                        //meal = await MealService().getMealById(1);
-                        //print(meal.toString());
-
-                        //  GETMEALBYDATE-TODAY//
-                        //DateTime now = DateTime.now();
-                        //String formattedDate =
-                        //    "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
-                        //print(formattedDate);
-                        //final List<MealModel> meals =
-                        //    await MealService().getMealByDate(formattedDate);
-                        //meals.length;
-
-                        // ADDFOODTORECIPE //
-                        //RecipeListService().addFoodToRecipe(1, 1);
-                        //RecipeListService().addFoodToRecipe(1, 2);
-
-                        // GETALLUSERRECIPES //
-                        //List<RecipeModel> list =
-                        //    await RecipeService().getalluserrecipe();
-                        //list.forEach((element) {
-                        //  print(element.toString());
-                        //});
-
-                        // GETALLCOMMENTS WITH RECIPE//
-                        //await CommentService().getAllComments();
-
-                        // ** PRUEBA DE ENDPOINTS //
-                      } else {
-                        CustomToast.customToast(
-                            'Email or password incorrect', context);
-                        Navigator.pushReplacementNamed(context, 'login');
-                      }
-                    },
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: Text(loginForm.isLoading ? 'Espere' : 'Ingresar',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.black45,
-                    )),
-              )),
         ],
       ),
     );
--- NutriGainsFront/nutrigains_front/lib/screens/register_screen.dart
+++ NutriGainsFront/nutrigains_front/lib/screens/register_screen.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: use_build_context_synchronously, avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/register_provider.dart';
@@ -17,37 +14,51 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: AuthBackground(
-            child: SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 250),
-          CardContainer(
-              child: Column(
+      resizeToAvoidBottomInset: true,
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              const SizedBox(height: 10),
-              Text('Crear cuenta',
-                  style: Theme.of(context).textTheme.headline4),
-              const SizedBox(height: 30),
-              ChangeNotifierProvider(
-                  create: (_) => RegisterFormProvider(), child: _RegisterForm())
-            ],
-          )),
-          const SizedBox(height: 50),
-          TextButton(
-              onPressed: () => Navigator.pushReplacementNamed(context, 'login'),
-              style: ButtonStyle(
+              const SizedBox(height: 250),
+              CardContainer(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Crear cuenta',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 60),
+                    ChangeNotifierProvider(
+                      create: (_) => RegisterFormProvider(),
+                      child: _RegisterForm(),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              TextButton(
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, 'login'),
+                style: ButtonStyle(
                   overlayColor:
                       MaterialStateProperty.all(Colors.indigo.withOpacity(0.1)),
-                  shape: MaterialStateProperty.all(const StadiumBorder())),
-              child: const Text(
-                '¿Ya tienes una cuenta?',
-                style: TextStyle(fontSize: 18, color: Colors.black87),
-              )),
-          const SizedBox(height: 50),
-        ],
+                  shape: MaterialStateProperty.all(const StadiumBorder()),
+                ),
+                child: const Text(
+                  '¿Ya tienes una cuenta?',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
       ),
-    )));
+    );
   }
 }
 
@@ -56,50 +67,57 @@ class _RegisterForm extends StatelessWidget with InputValidationMixin {
   Widget build(BuildContext context) {
     final registerForm = Provider.of<RegisterFormProvider>(context);
 
-    return Form(
-      key: registerForm.formKey,
-      // autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: Column(
-        // ignore: sort_child_properties_last
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.name,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: '', labelText: 'Username', prefixIcon: Icons.person),
-            onChanged: (value) => registerForm.username = value,
-            validator: (name) {
-              if (isTextValid(name)) {
-                return null;
-              } else {
-                return 'Name field cant be null';
-              }
-            },
-          ),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            keyboardType: TextInputType.text,
-            decoration: InputDecorations.authInputDecoration(
+    return SingleChildScrollView(
+      // Utiliza SingleChildScrollView como contenedor principal
+      child: Form(
+        key: registerForm.formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.name,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '',
+                labelText: 'Username',
+                prefixIcon: Icons.person,
+              ),
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+              onChanged: (value) => registerForm.username = value,
+              validator: (name) {
+                if (isTextValid(name)) {
+                  return null;
+                } else {
+                  return 'Name field cant be null';
+                }
+              },
+            ),
+            const SizedBox(height: 40),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              decoration: InputDecorations.authInputDecoration(
                 hintText: '*******',
                 labelText: 'Contraseña',
-                prefixIcon: Icons.lock_outline),
-            onChanged: (value) => registerForm.password = value,
-            validator: (password) {
-              if (isPasswordValid(password)) {
-                return null;
-              } else {
-                return 'Enter a valid password';
-              }
-            },
-          ),
-          const SizedBox(height: 30),
-          MaterialButton(
+                prefixIcon: Icons.lock_outline,
+              ),
+              onChanged: (value) => registerForm.password = value,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+              validator: (password) {
+                if (isPasswordValid(password)) {
+                  return null;
+                } else {
+                  return 'Enter a valid password';
+                }
+              },
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
+                borderRadius: BorderRadius.circular(10),
+              ),
               disabledColor: Colors.grey,
-              elevation: 0,
-              color: Colors.deepPurple,
+              color: Colors.amber,
               onPressed: registerForm.isLoading
                   ? null
                   : () async {
@@ -111,7 +129,6 @@ class _RegisterForm extends StatelessWidget with InputValidationMixin {
 
                       registerForm.isLoading = true;
 
-                      //validar si el login es correcto
                       final String? errorMessage = await authService.register(
                         registerForm.username,
                         registerForm.password,
@@ -125,21 +142,24 @@ class _RegisterForm extends StatelessWidget with InputValidationMixin {
                         CustomToast.customToast(
                             'Este usuario ya existe', context);
                         Navigator.pushReplacementNamed(context, 'register');
-                        //mostrar error en pantalla
-                        // customToast('The email is already registered', context);
                         registerForm.isLoading = false;
                         print(errorMessage);
                       }
-                      // }
                     },
               child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                  child: Text(
-                    registerForm.isLoading ? 'Espere' : 'Registrar',
-                    style: const TextStyle(color: Colors.white),
-                  )))
-        ],
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  registerForm.isLoading ? 'Espere' : 'Registrar',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
